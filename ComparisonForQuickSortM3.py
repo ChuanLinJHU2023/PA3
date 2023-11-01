d7be0acfe903bc361989b5037d2d95937bd13751
NotAClue130 <145219499+NotAClue130@users.noreply.github.com>
2023-11-01 09:33:25 +0800

    The 11th edition!!

--- ComparisonForQuickSortM3.py
+++ ComparisonForQuickSortM3.py
@@ -1,11 +1,9 @@
-# ComparisonForQuickSortM3.py draws the graph to compare algorithm’s worst-case asymptotic behavior to the achieved
-# asymptotic behavior of the program
-
 import matplotlib.pyplot as plt
-import matplotlib
+import matplotlib as mpl
 
-matplotlib.use('TkAgg')
-filename_of_graph = "ComparisonForQuickSort_Output"
+mpl.use('TkAgg')  # Under the new MacOS, we need this line to make sure that matplotlib works properly
+
+filename_of_graph = "ComparisonForQuickSortM3_Output"
 
 
 def reader_for_record(record_filename):
@@ -17,7 +15,7 @@ def reader_for_record(record_filename):
         return lines
 
 
-record_filename = "TestRunForQuickSort_Output"
+record_filename = "TestRunForQuickSortM3_Output"
 records = reader_for_record(record_filename)
 list_n = [single_record[0] for single_record in records][1:]
 list_TotalNumberOfExecutionsOfPartition = [single_record[1] for single_record in records][1:]
@@ -27,13 +25,19 @@ list_Expression1 = [single_record[4] for single_record in records][1:]
 list_Expression2 = [single_record[5] for single_record in records][1:]
 list_Expression3 = [single_record[6] for single_record in records][1:]
 
-# print(list_n)
+print(list_n)
 print(list_TotalNumberOfExecutionsOfPartition)
-plt.plot(list_n, list_TotalNumberOfExecutionsOfPartition, label="TotalNumberOfExecutionsOfPartition")
+plt.plot(list_n, list_TotalNumberOfExecutionsOfPartition, "b", label="TotalNumberOfExecutionsOfPartition")
+plt.plot(list_n, list_Expression1,"b--", label="n/2")
+plt.plot(list_n, list_TotalNumberOfExecutionsOfQuickSort, "g", label="TotalNumberOfExecutionsOfQuickSort")
+plt.plot(list_n, list_Expression2,"g--", label="n")
+plt.plot(list_n, list_TotalNumberOfComparisonsWithinPartition, "r", label='TotalNumberOfComparisonsWithinPartition')
+plt.plot(list_n, list_Expression3,"r--", label="n*n/5")
 
 plt.xlabel("n")
-plt.ylabel("TotalNumber")
+plt.ylabel("count")
 plt.legend()
-plt.title("n versus TotalNumber".format(m))
-plt.savefig(filename_of_graph, dpi=1500)
+plt.title("Asymptotical Behavior (n versus counts)")
+plt.savefig(filename_of_graph)
+# plt.savefig(filename_of_graph, dpi=1500)
 plt.show()
